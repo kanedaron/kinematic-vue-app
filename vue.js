f5a4543930c6b94f1a7a6c3c2e81b3218c8e8a50
kanedaron <betasarkozy@live.fr>
2020-06-05 12:29:49 +0200

    Finished almost all the logic and style, only remains the animation

--- vue.js
+++ vue.js
@@ -4,10 +4,18 @@ var app1 = new Vue({
     cinema: [],
     filters: {},
     error: false,
-    selectedMovie:false
+    selectedMovie:false,
+    loading:true,
+  },
+  computed: {
+      cinema2: function () {
+        if (this.loading===false)
+        return app2.computedList
+        return this.cinema
+      }
   },
   created: function () {
-    const loading = this.$vs.loading({ type: "square",color:"#ffa500" });
+    const loading = this.$vs.loading({ type: "square",color:"#ffa500",text:"loading..." });
     fetch(
       "https://raw.githubusercontent.com/StreamCo/react-coding-challenge/master/feed/sample.json"
     )
@@ -16,6 +24,7 @@ var app1 = new Vue({
         (result) => {
           this.cinema = result.entries;
           loading.close();
+          this.loading=false;
         },
         
         (error) => {
@@ -23,14 +32,30 @@ var app1 = new Vue({
           this.error = error;
         }
       );
-  },
+  }
+  
 });
 
 var app2 = new Vue({
   el: "#app2",
   data: {
-    value: "",
+    search: "",
     value2: "",
     value3: "",
+  },
+  computed: {
+    computedList: function () {
+      let list = app1.cinema
+      if (app2.search !== "") {
+        const regex2 = new RegExp(`${app2.search.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&")}`, "i");
+        list = list.filter((movie) => movie.title.search(regex2) !== -1);
+      }
+      return list
+    },
+    // a computed getter
+    escapedSearch: function () {
+      // `this` points to the vm instance
+      return this.search.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&");
+    },
   },
 });
